5326d511fae5885a9b42dcf38dfb49deabacdb48
lemontree2000 <gonelove.guo@gmail.com>
2019-12-23 18:48:26 +0800

    update

---
+++ ui/src/utils/cos.js
@@ -0,0 +1,47 @@
+import COS from "cos-js-sdk-v5";
+
+class CosUtil {
+  config = {};
+  cos = {};
+  constructor() {
+    this.init();
+  }
+  /**
+   * 初始化
+   */
+  init() {
+    window.chrome.storage.sync.get("userConfig", storage => {
+      this.config = storage.userConfig;
+      this.cos = new COS({
+        SecretId: this.config.SecretId,
+        SecretKey: this.config.SecretKey
+      });
+    });
+  }
+
+  /**
+   * 上传文件
+   * @param {File} file 文件对象
+   * @param {Object} options 回调选项
+   *   @param {Function} options.onTaskReady
+   *   @param {Function} options.onHashProgress
+   *   @param {Function} options.onProgress
+   * @param {Function} cb 上传完成，成功或者失败
+   */
+  uploadFile(file, options, cb) {
+    this.cos.sliceUploadFile(
+      {
+        Bucket: this.config.Bucket, // Bucket 格式：test-1250000000
+        Region: this.config.Region,
+        Key: "Picture-Portal/" + file.name,
+        Body: file,
+        onTaskReady: options.onTaskReady,
+        onHashProgress: options.onHashProgress,
+        onProgress: options.onProgress
+      },
+      cb
+    );
+  }
+}
+
+export default new CosUtil();
--- ui/src/views/Popup.vue
+++ ui/src/views/Popup.vue
@@ -1,33 +1,63 @@
 <template>
   <div class="popup-page">
     <div class="bg">
-      <h1><img src="../assets/portal.png" alt="" width="30"> Picture Portal</h1>
+      <h1>
+        <img src="../assets/portal.png" alt width="30" /> Picture Portal
+      </h1>
     </div>
-    <input type="file" @change="handleFileChange" />
+    <div class="upload-tasks">
+      <ul>
+        <li class="task-item">
+          <div class="file-input">
+            <input type="file" accept=".jpg, .png, .gif" @change="handleFileChange" />
+            <img :src="src" width="100%" alt />
+          </div>
+          <ul class="url-types">
+            <li class="url-item">
+              <span>URL</span>
+              <input readonly type="text" placeholder="普通url" />
+              <div>复制</div>
+            </li>
+            <li class="url-item">
+              <span>HTML</span>
+              <input readonly type="text" placeholder="html格式" />
+              <div>复制</div>
+            </li>
+            <li class="url-item">
+              <span>Markdown</span>
+              <input readonly type="text" placeholder="md格式" />
+              <div>复制</div>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-import COS from "cos-js-sdk-v5";
-const cos = new COS({
-  SecretId: "",
-  SecretKey: ""
-});
+import cosInstance from "../utils/cos";
 
 export default {
+  data() {
+    return {
+      src: ""
+    };
+  },
   created() {
-    console.log(cos);
     document.title = "Picture Portal - 个人云图床";
   },
   methods: {
     handleFileChange(e) {
       const file = e.target.files[0];
-      cos.sliceUploadFile(
+      const reader = new FileReader();
+      reader.onload = e => {
+        this.src = e.target.result;
+      };
+      reader.readAsDataURL(file);
+      cosInstance.uploadFile(
+        file,
         {
-          Bucket: "", // Bucket 格式：test-1250000000
-          Region: "",
-          Key: "lemontree2000/" + file.name,
-          Body: file,
           onTaskReady: function(tid) {
             console.log(tid);
           },
@@ -38,7 +68,7 @@ export default {
             console.log("onProgress", JSON.stringify(progressData));
           }
         },
-        function(err, data) {
+        (err, data) => {
           console.log(err || data);
         }
       );
@@ -48,8 +78,19 @@ export default {
 </script>
 
 <style scoped>
+ul,
+li {
+  padding: 0;
+  margin: 0;
+  list-style: none;
+}
 .popup-page {
   user-select: none;
+  max-width: 80%;
+  margin: 0 auto;
+}
+.popup-page .bg {
+  height: 60px;
 }
 .popup-page h1 {
   text-align: center;
@@ -58,5 +99,54 @@ export default {
 }
 .popup-page h1 img {
   vertical-align: text-bottom;
+}
+.upload-tasks .task-item,
+.url-types .url-item {
+  color: #333;
+  font-size: 14px;
+  display: flex;
+  width: 100%;
+}
+.url-types {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  flex: 1;
+}
+.task-item .file-input {
+  width: 120px;
+  height: 120px;
+  border-radius: 3px;
+  border: 1px solid #ccc;
+}
+.file-input input {
+  opacity: 0;
+  display: block;
+  width: 100%;
+  height: 100%;
+}
+.url-item span {
+  width: 90px;
+  text-align: right;
+}
+.url-item input {
+  border: none;
+  border-bottom: 1px solid #dedede;
+  flex: 1;
+  margin: 0 10px;
+}
+.url-item div {
+  width: 45px;
+  height: 25px;
+  font-size: 12px;
+  line-height: 25px;
+  text-align: center;
+  background: #36f;
+  color: #fff;
+  border-radius: 2px;
+  cursor: pointer;
+}
+.url-item input:focus {
+  outline: none;
 }
 </style>
